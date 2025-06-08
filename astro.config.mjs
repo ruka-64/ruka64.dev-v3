94e4cff711369d2dc2686aa6dbaeb8268ab88373
ruka-64 <202770393+ruka-64@users.noreply.github.com>
2025-06-08 15:47:59 +0900

    noscript, static

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
+	output: "static",
 	vite: {
 		plugins: [tailwindcss()],
 	},
